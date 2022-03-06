4fada8b0a12973e8cfa79972ab04ad8ea93d4215
Kenneth Fan <kf6999@rit.edu>
2022-03-06 12:12:40 -0500

    Commit initial website

---
+++ src/routes/index.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import Typewriter from 'svelte-typewriter'
+</script>
+
+<main>
+  <div class="flex h-screen justify-center mt-20">
+    <div class="text-center">
+      <Typewriter interval={75} cursor={false}>
+        <h1 class="text-5xl font-semibold">Kenneth Fan</h1>
+      </Typewriter>
+
+        <p class="mt-2">Fouth year CIT student at <a target="_blank" rel="noopener noreferrer" href="http://rit.edu">RIT</a></p>
+        <button class="btn btn-success btn-xs mt-3">Currently Looking</button>
+
+    <div class="mt-10 space-y-2">      
+        
+      <a target="_blank" rel="noopener noreferrer" href="http://your_url_here.html">Resume</a><p></p>
+      <a target="_blank" rel="noopener noreferrer" href="https://github.com/kf6999">GitHub</a><p></p>
+      <p>Email</p>
+
+      </div>
+    </div>
+  </div>
+
+</main>
+
+<style>
+
+
+</style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [
+    require('daisyui')
+  ],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dracula",
+  },
+};
