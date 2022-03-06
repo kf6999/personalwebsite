1a8df20da578aa134a10f2a59fda0c12dce827a8
Kenneth Fan <kf6999@rit.edu>
2022-03-06 12:29:30 -0500

    Fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,10 +8,8 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
-			// ... truncated ...
-			target: '#svelte'
-		  }
+		adapter: adapter()
+	}
 };
 
 export default config;
